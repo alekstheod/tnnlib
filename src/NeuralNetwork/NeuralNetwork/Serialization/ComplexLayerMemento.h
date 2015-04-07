b9caf365eda842c7e4e797002d1df67e5e9b25e6
alekstheod@gmail.com <alekstheod@gmail.com>
2015-04-07 22:40:51 +0200

    Preparation for a deep learning algorithm implementation. Added a special wrapper for a perceptron (adapting to the neural layer interface). The idea is to use a perceptron as autoencoder.

---
+++ src/NeuralNetwork/NeuralNetwork/Serialization/ComplexLayerMemento.h
@@ -0,0 +1,34 @@
+#ifndef COMPLEXLAYER_MEMENTO_H
+#define COMPLEXLAYER_MEMENTO_H
+#include <NeuralNetwork/Serialization/PerceptronMemento.h>
+
+namespace nn {
+
+template<typename Var>
+class ComplexLayerMemento {
+private:
+    PerceptronMemento<Var> m_perceptron;
+
+    friend class boost::serialization::access;
+
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version)
+    {
+        ar & BOOST_SERIALIZATION_NVP(m_perceptron);
+    }
+    
+public:
+  const PerceptronMemento<Var>& getPerceptron()const{
+    return m_perceptron;
+  }
+  
+  void setPerceptron(const PerceptronMemento<Var>& perceptron ){
+    m_perceptron = m_perceptron;
+  }
+  
+  ~ComplexLayerMemento(){}
+};
+
+}
+
+#endif
