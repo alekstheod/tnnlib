5af8b3de8013470b391fb4be9593f07fdf446e27
grisha vanika <grish.vanika@gmail.com>
2018-06-24 23:41:03 +0300

    Fix few MSVC warnings.
    make Catch as INTERFACE library (for simpler warnings configuration)

--- src/libs/range-v3_integration.cmake
+++ src/libs/range-v3_integration.cmake
@@ -4,6 +4,10 @@ add_library(range-v3 INTERFACE)
 if (${MSVC})
 	target_include_directories(range-v3 INTERFACE
 		${CMAKE_CURRENT_SOURCE_DIR}/range-v3/win)
+	target_compile_options(range-v3 INTERFACE
+		# '+=': conversion from '__int64' to 'int', possible loss of data
+		# (fixed in the original library)
+		"/wd4244")
 else ()
 	target_include_directories(range-v3 INTERFACE
 		${CMAKE_CURRENT_SOURCE_DIR}/range-v3/nix)
