59bc4dc830d3f86998c422048b9e090c6eabd7eb
alekstheod <alekstheod@gmail.com>
2022-07-05 19:04:45 +0200

    cleanup perceptron

---
+++ Utilities/MPL/Algorithm.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <utility>
+#include <vector>
+
+namespace utils {
+
+    template< std::size_t N >
+    struct num {
+        static const constexpr auto value = N;
+    };
+
+    template< class F, std::size_t... Is >
+    void for_(F func, std::index_sequence< Is... >) {
+        using expander = int[];
+        (void)expander{0, ((void)func(num< Is >{}), 0)...};
+    }
+
+    template< std::size_t N, typename F >
+    void for_(F func) {
+        for_(func, std::make_index_sequence< N >());
+    }
+
+    template< std::size_t idx, typename T >
+    auto& get(T& container) {
+        return std::get< idx >(container);
+    }
+
+    template< std::size_t idx, typename T >
+    const auto& get(const T& container) {
+        return std::get< idx >(container);
+    }
+
+    template< std::size_t idx, typename T >
+    auto& get(std::vector< T >& v) {
+        return v[idx];
+    }
+
+    template< std::size_t idx, typename T >
+    const auto& get(const std::vector< T >& v) {
+        return v[idx];
+    }
+} // namespace utils
