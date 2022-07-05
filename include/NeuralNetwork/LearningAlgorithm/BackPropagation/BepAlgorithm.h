02ef175a77dc6435c1e7d9f6b935248c804f8b1d
alekstheod <alekstheod@gmail.com>
2022-07-05 18:36:44 +0200

    cleanup include guards

--- include/NeuralNetwork/LearningAlgorithm/BackPropagation/BepAlgorithm.h
+++ include/NeuralNetwork/LearningAlgorithm/BackPropagation/BepAlgorithm.h
@@ -1,34 +1,5 @@
-/**
-*  Copyright (c) 2011, Alex Theodoridis
-*  All rights reserved.
+#pragma once
 
-*  Redistribution and use in source and binary forms, with
-*  or without modification, are permitted provided that the
-*  following conditions are met:
-*  Redistributions of source code must retain the above
-*  copyright notice, this list of conditions and the following disclaimer.
-*  Redistributions in binary form must reproduce the above
-*  copyright notice, this list of conditions and the following
-*  disclaimer in the documentation and/or other materials
-*  provided with the distribution.
-
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
-*  AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-*  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-*  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-*  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-*  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-*  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-*  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-*  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-*  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-*  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
-*/
-
-#ifndef BEPTrainerH
-#define BEPTrainerH
 #include <NeuralNetwork/LearningAlgorithm/BackPropagation/BPNeuralLayer.h>
 #include <NeuralNetwork/LearningAlgorithm/BackPropagation/ErrorFunction.h>
 #include <NeuralNetwork/Perceptron/Perceptron.h>
@@ -136,10 +107,8 @@ namespace nn {
                         return init + executeTrainingStep(first, momentum);
                     };
 
-                    error = std::accumulate(prototypes.begin(),
-                                            prototypes.end(),
-                                            Var{},
-                                            runTrainingStep);
+                    error =
+                     std::accumulate(prototypes.begin(), prototypes.end(), Var{}, runTrainingStep);
 
                 } while(errorFunc(++epochCounter, error));
 
@@ -179,5 +148,3 @@ namespace nn {
         };
     } // namespace bp
 } // namespace nn
-
-#endif
