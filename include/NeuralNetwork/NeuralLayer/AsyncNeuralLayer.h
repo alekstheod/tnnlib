c61e7db33e862024bf475eb0611049cc88cf4519
alekstheod <alekstheod@gmail.com>
2023-03-11 20:35:31 +0100

    Add asyncLayer

---
+++ include/NeuralNetwork/NeuralLayer/AsyncNeuralLayer.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <NeuralNetwork/NeuralLayer/NeuralLayer.h>
+
+#include <thread>
+#include <future>
+
+namespace nn {
+
+    namespace detail {
+        template< typename Internal >
+        struct AsyncNeuralLayer : private Internal {
+            using Internal::adjust;
+            using Internal::begin;
+            using Internal::cbegin;
+            using Internal::cend;
+            using Internal::end;
+            using Internal::for_each;
+            using Internal::getMemento;
+            using Internal::getOutput;
+            using Internal::inputs;
+            using Internal::Memento;
+            using Var = typename Internal::Var;
+            using Internal::operator[];
+            using Internal::setInput;
+            using Internal::setMemento;
+            using Internal::size;
+            using Internal::use;
+            using Internal::wrap;
+
+            /**
+             * @see {INeuralLayer}
+             */
+            template< typename Layer >
+            void calculateOutputs(Layer& nextLayer) {
+                calculateOutputs();
+                for(unsigned int i = 0; i < size(); i++) {
+                    nextLayer.setInput(i, operator[](i).getOutput());
+                }
+            }
+
+            void calculateOutputs() {
+                std::array< std::future< Var >, size() > dotProducts;
+
+                utils::for_< size() >([this, &dotProducts](auto i) {
+                    auto& neuron = operator[](i.value);
+                    dotProducts[i.value] =
+                     std::async([&neuron]() { return neuron.calcDotProduct(); });
+                });
+
+                std::array< Var, size() > products;
+                std::transform(std::begin(dotProducts),
+                               std::end(dotProducts),
+                               std::begin(products),
+                               [](auto& f) { return f.get(); });
+                for_each([&products](auto, auto& neuron) {
+                    neuron.calculateOutput(std::cbegin(products), std::cend(products));
+                });
+            }
+        };
+    } // namespace detail
+
+    /// @brief neural layer (utilizes thread pool to compute a dot product)
+    /// @param NeuronType a type of the neuron in a layer.
+    /// @param ActivationFunction a type of the activation function used in a
+    /// neuron.
+    /// @param size ammount of neurons in a layer.
+    /// @param inputsNumber the number of inputs of each neuron in a layer.
+    /// initialization a final weight will be calculated in a following way
+    /// random(0, 1)/scaleFactor
+    template< template< template< class > class, class, std::size_t > class NeuronType,
+              template< class >
+              class ActivationFunctionType,
+              std::size_t size,
+              std::size_t inputsNumber = 2,
+              typename Var = float >
+    using AsyncNeuralLayer =
+     detail::AsyncNeuralLayer< NeuralLayer< NeuronType, ActivationFunctionType, size, inputsNumber > >;
+} // namespace nn
---
+++ include/NeuralNetwork/NeuralLayer/tests/AsyncNeuralLayerTest.cpp
@@ -0,0 +1,32 @@
+#include <NeuralNetwork/NeuralLayer/AsyncNeuralLayer.h>
+#include <NeuralNetwork/NeuralLayer/NeuralLayer.h>
+#include <NeuralNetwork/ActivationFunction/TanhFunction.h>
+#include <NeuralNetwork/Neuron/Neuron.h>
+
+#include <range/v3/all.hpp>
+
+#define CATCH_CONFIG_NO_CPP17_UNCAUGHT_EXCEPTIONS
+#include <catch2/catch.hpp>
+
+namespace {
+    SCENARIO("AsyncNeuralLayer compared to regular NeuralLayer",
+             "[layer][opencl][forward]") {
+        GIVEN(
+         "A AsyncNeuralLayer layer with 2 neurons and 2 inputs as well as a "
+         "regular layer with the same topology") {
+            nn::NeuralLayer< nn::Neuron, nn::TanhFunction, 2, 2 > regularLayer;
+            const auto memento = regularLayer.getMemento();
+            nn::AsyncNeuralLayer< nn::Neuron, nn::TanhFunction, 2, 2 > asyncLayer;
+            asyncLayer.setMemento(memento);
+            WHEN("The weights and the inputs of both layers are identical") {
+                THEN("The output is of both layers identical") {
+                    regularLayer.calculateOutputs();
+                    asyncLayer.calculateOutputs();
+                    const auto expected_output = regularLayer.getOutput(0);
+                    const auto actual_output = asyncLayer.getOutput(0);
+                    REQUIRE(expected_output == actual_output);
+                }
+            }
+        }
+    }
+} // namespace
