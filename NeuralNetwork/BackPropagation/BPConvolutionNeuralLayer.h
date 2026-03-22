9b76e9c390565e1fe471327895c00fd2af3fcd9f
Alex Theodoridis <alekstheod@gmail.com>
2026-03-22 19:54:01 +0100

    Fix

--- NeuralNetwork/BackPropagation/BPConvolutionNeuralLayer.h
+++ NeuralNetwork/BackPropagation/BPConvolutionNeuralLayer.h
@@ -70,9 +70,6 @@ namespace nn::bp {
             auto& self = *this;
             return self[neuronId].getDelta();
         }
-
-      private:
-        Grid m_grid;
     };
 
 } // namespace nn::bp
